1aeb91bada3f3f3f417caddc3dec26c0deeaad36
Muhammad Owais <rahneverd@gmail.com>
2024-03-23 23:23:01 +0300

    sessions code

--- index.ts
+++ index.ts
@@ -1,18 +1,40 @@
 import inquirer from "inquirer";
 
-const answer = await inquirer.prompt([{
+let answers = await inquirer.prompt([
+  {
     name: 'firstNumber',
-      message: 'Enter first number',
-      type: 'input'
-     },{
-      name: 'SecondNumber',
-        message: 'Enter second number',
-        type: 'input'
-       },{
-        name: 'operator',
-          message: 'Enter operator',
-          type: 'list',
-          choices: ['add', 'subtract', 'multiply', 'devide']
-         }])
+    message: 'Enter first number',
+    type: 'number'
+  }, {
+    name: 'secondNumber',
+    message: 'Enter second number',
+    type: 'number'
+  }, {
+    name: 'operator',
+    message: 'Select Operator',
+    type: 'list',
+    choices: ['Add', 'Subtract', 'Mutiply', 'Divide']
+  }
+])
 
-     console.log(answer)
+console.log(answers)
+let result: number
+if (answers.operator === 'Add') {
+  // Addition
+  result = answers.firstNumber + answers.secondNumber
+  
+} else if (answers.operator === 'Subtract') {
+  // Subtraction
+  result = answers.firstNumber - answers.secondNumber
+
+}else if (answers.operator === 'Mutiply') {
+  // Multiplication
+  result = answers.firstNumber * answers.secondNumber
+
+}else {
+  // Division
+  result = answers.firstNumber / answers.secondNumber
+
+}
+
+console.log(`Result is ${result}`)
