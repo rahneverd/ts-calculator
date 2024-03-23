1ba3867bcb2f0afe21f0ef654e43a55d79b3a554
Muhammad Owais <rahneverd@gmail.com>
2024-03-23 13:29:41 +0300

    added questions

---
+++ index.ts
@@ -0,0 +1,18 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([{
+    name: 'firstNumber',
+      message: 'Enter first number',
+      type: 'input'
+     },{
+      name: 'SecondNumber',
+        message: 'Enter second number',
+        type: 'input'
+       },{
+        name: 'operator',
+          message: 'Enter operator',
+          type: 'list',
+          choices: ['add', 'subtract', 'multiply', 'devide']
+         }])
+
+     console.log(answer)
